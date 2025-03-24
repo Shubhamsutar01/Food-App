79ed539fcff729a6c3341865fadb9ccb2fa7123e
Shubhamsutar01 <141011776+Shubhamsutar01@users.noreply.github.com>
2025-03-24 15:15:19 +0530

    Update Menu.jsx

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -47,39 +47,48 @@ export default function Menu() {
       
       <h2 style={{textDecoration:"underline"}} className='name'>Starters</h2>
         
+        <div className='menu'>
          <h4>1.Masala papad : {qty1}</h4>
          <p>(50/-)</p>
          <button onClick={()=>setqty1(qty1-1)}className='buttons'>-</button><button onClick={()=>setqty1(qty1+1)}className='buttons'>+</button>
+         </div>
          
+         <div className='menu'>
          <h4>2.Paneer tikka : {qty2}</h4>
          <p>(180/-)</p>
          <button onClick={()=>setqty2(qty2-1)} className='buttons'>-</button><button onClick={()=>setqty2(qty2+1)} className='buttons'>+</button>
-         
+         </div>
+         <div className='menu'>
          <h4>3.Noodles : {qty3}</h4>
          <p>(160/-)</p>
          <button onClick={()=>setqty3(qty3-1)} className='buttons'>-</button><button onClick={()=>setqty3(qty3+1)} className='buttons'>+</button>
-         
+         </div>
+         <div className='menu'>
          <h4>4.Veg Manchurian : {qty4}</h4>
          <p>(150/-)</p>
          <button onClick={()=>setqty4(qty4-1)} className='buttons'>-</button><button onClick={()=>setqty4(qty4+1)} className='buttons'>+</button>
-         
+         </div>
+         <div className='menu'>
          <h4>5.Crispy corn chat : {qty5}</h4>
          <p>(160/-)</p>
          <button onClick={()=>setqty5(qty5-1)} className='buttons'>-</button><button onClick={()=>setqty5(qty5+1)} className='buttons'>+</button>
-         
+         </div>
+         <div className='menu'>
          <h4>6.Dahi ke kabab : {qty6}</h4>
          <p>(190/-)</p>
          <button onClick={()=>setqty6(qty6-1)} className='buttons'>-</button><button onClick={()=>setqty6(qty6+1)} className='buttons'>+</button>
-         
+         </div>
+         <div className='menu'>
          <h4>7.Bharwan Aloo : {qty7}</h4>
          <p>(180/-)</p>
          <button onClick={()=>setqty7(qty7-1)} className='buttons'>-</button><button onClick={()=>setqty7(qty7+1)} className='buttons'>+</button>
-         
+         </div>
 
          <br/><br/>
-       
+       <div className='total'>
        <h2>Starters:{total}</h2>
         <button onClick={handleSubmit1}>Main course</button>
+        </div>
         {/* <button onClick={handleSubmit3}>Paneer</button> */}
        
         {/* <button onClick={handleSubmit2}>Submit</button> */}
@@ -140,58 +149,68 @@ export function Home() {
         <div className='div1'>
           
           <h2 style={{textDecoration:"underline"}} className='name'>Main Course</h2>
-        
+          <div className='menu'>
         <h4>1.Paneer Btr Masala : {qty1}</h4>
         <p>(450/-)</p>
         <button onClick={()=>setqty1(qty1-1)}className='buttons'>-</button><button onClick={()=>setqty1(qty1+1)}className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>2.Shahi Paneer : {qty2}</h4>
         <p>(475/-)</p>
         <button onClick={()=>setqty2(qty2-1)} className='buttons'>-</button><button onClick={()=>setqty2(qty2+1)} className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>3.Paneer Burji : {qty3}</h4>
         <p>(460/-)</p>
         <button onClick={()=>setqty3(qty3-1)} className='buttons'>-</button><button onClick={()=>setqty3(qty3+1)} className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>4.Paneer Tava masal : {qty4}</h4>
         <p>(480/-)</p>
         <button onClick={()=>setqty4(qty4-1)} className='buttons'>-</button><button onClick={()=>setqty4(qty4+1)} className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>5.Dum Aloo : {qty5}</h4>
         <p>(450/-)</p>
         <button onClick={()=>setqty5(qty5-1)} className='buttons'>-</button><button onClick={()=>setqty5(qty5+1)} className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>6.Kaju curry : {qty6}</h4>
         <p>(500/-)</p>
         <button onClick={()=>setqty6(qty6-1)} className='buttons'>-</button><button onClick={()=>setqty6(qty6+1)} className='buttons'>+</button>
-
-          <h2 style={{textDecoration:"underline"}} className='name'>Tandoori Delights</h2>
+        </div>
+          {<h2 /* style={{textDecoration:"underline" , backgroundColor:"#a5bdfd", padding:"10px"}}*/ className='name'>Tandoori Delights</h2> }
         
+          <div className='menu'>
           <h4>1.Butter Naan : {qty7}</h4>
         <p>(50/-)</p>
         <button onClick={()=>setqty7(qty7-1)} className='buttons'>-</button><button onClick={()=>setqty7(qty7+1)} className='buttons'>+</button>
-
+        </div>
+        <div className='menu'>
         <h4>2.Garlic Naan : {qty8}</h4>
         <p>(55/-)</p>
         <button onClick={()=>setqty8(qty8-1)} className='buttons'>-</button><button onClick={()=>setqty8(qty8+1)} className='buttons'>+</button>
-
+        </div>
+        <div className='menu'>
         <h4>3.Tandoori Roti : {qty9}</h4>
         <p>(40/-)</p>
         <button onClick={()=>setqty9(qty9-1)} className='buttons'>-</button><button onClick={()=>setqty9(qty9+1)} className='buttons'>+</button>
-
+        </div>
+        <div className='menu'>
         <h4>4.Roomali Roti : {qty10}</h4>
         <p>(55/-)</p>
         <button onClick={()=>setqty10(qty10-1)} className='buttons'>-</button><button onClick={()=>setqty10(qty10+1)} className='buttons'>+</button>
-
+         </div>
 
 
         <br/><br/>
-        
+        <div className='total'> 
       <h2>Main Course :{maintotal}</h2>
       <h4>Starters :{total}</h4>
+      
        {/* <button onClick={handleSubmit1}>Starter</button> */}
        <button onClick={handleSubmit3}>Drinks&Dessert</button>
-     
+       </div>
        {/* <button onClick={handleSubmit2}>Submit</button> */}
    </div>
     )
@@ -202,6 +221,7 @@ export function Home() {
     const navigate= useNavigate();
 
     const refresh=()=>{
+      alert("Payment Received !")
         navigate("/")
         
         
@@ -271,53 +291,71 @@ export function Home() {
     return (
         <div className='div1'><h2 style={{textDecoration:"underline"}} className='name'>Drinks</h2>
         
+        <div className='menu'>
         <h4>1.Virgin Mojito : {qty1}</h4>
         <p>(170/-)</p>
         <button onClick={()=>setqty1(qty1-1)}className='buttons'>-</button><button onClick={()=>setqty1(qty1+1)}className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>2.Blue Lagoon : {qty2}</h4>
         <p>(180/-)</p>
         <button onClick={()=>setqty2(qty2-1)} className='buttons'>-</button><button onClick={()=>setqty2(qty2+1)} className='buttons'>+</button>
-        
-        <h4>3.Watermelon Basil Fizz : {qty3}</h4>
+        </div>
+        <div className='menu'>
+        <h4>3.Watermelon Fizz : {qty3}</h4>
         <p>(200/-)</p>
         <button onClick={()=>setqty3(qty3-1)} className='buttons'>-</button><button onClick={()=>setqty3(qty3+1)} className='buttons'>+</button>
-        
+        </div>
+        <div className='menu'>
         <h4>4.Mango Tango : {qty4}</h4>
         <p>(240/-)</p>
         <button onClick={()=>setqty4(qty4-1)} className='buttons'>-</button><button onClick={()=>setqty4(qty4+1)} className='buttons'>+</button>
-      
+        </div>
+        <div className='menu'>
         <h4>5.Berry Bliss : {qty5}</h4>
         <p>(250/-)</p>
         <button onClick={()=>setqty5(qty5-1)} className='buttons'>-</button><button onClick={()=>setqty5(qty5+1)} className='buttons'>+</button>
-      
-      <h2 style={{textDecoration:"underline"}} className='name'>Dessert </h2>
+        </div>
+
+      { <h2/* style={{textDecoration:"underline", backgroundColor:"#a5bdfd", padding:"10px"}} */className='name'>Dessert </h2> }
+       
+      <div className='menu'>
         <h4>1.Basundi : {qty6}</h4>
         <p>(200/-)</p>
         <button onClick={()=>setqty6(qty6-1)} className='buttons'>-</button><button onClick={()=>setqty6(qty6+1)} className='buttons'>+</button>
-      
+        </div>
+        <div className='menu'>
         <h4>2.Gajar Halwa : {qty7}</h4>
         <p>(200/-)</p>
         <button onClick={()=>setqty7(qty7-1)} className='buttons'>-</button><button onClick={()=>setqty7(qty7+1)} className='buttons'>+</button>
-      
+       </div>
+       <div className='menu'>
         <h4>3.Rasmalai : {qty8}</h4>
         <p>(220/-)</p>
         <button onClick={()=>setqty8(qty8-1)} className='buttons'>-</button><button onClick={()=>setqty8(qty8+1)} className='buttons'>+</button>
-       
+       </div>
+       <div className='menu'>
         <h4>4.Anjeer Halwa : {qty9}</h4>
         <p>(250/-)</p>
         <button onClick={()=>setqty9(qty9-1)} className='buttons'>-</button><button onClick={()=>setqty9(qty9+1)} className='buttons'>+</button>
-      
-
+      </div>
+        <div className='total'>
       <h2>For soul : {total}</h2>
       <h4>Starters + Main :{strtMainTotal}</h4>
        {/* <button onClick={handleSubmit1}>Starter</button> */}
        {/* <button onClick={handleSubmit3}>Paneer</button> */}
      
-       <button onClick={handleSubmit2}>Order</button>
+       <button onClick={handleSubmit2}>Place Order</button>
+         </div>
    </div>
     )
   }
+  
+ export function Thanks(){
+  return(
+    <h1>Thank You</h1>
+  )
+ }
   
  export function Thanks(){
   return(
